60591d60243392e30d69187497611267c74244d5
Michaelli8899 <147345359+Michaelli8899@users.noreply.github.com>
2024-09-09 10:12:09 -0400

    Edit Radio Button & Highlight
    Add css styleing to .selector-button label{} to align the text. Allow highlight on product in nav

--- solution-hw0/src/App.js
+++ solution-hw0/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Homework 0 Completed</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
